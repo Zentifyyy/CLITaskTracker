a423506e996e0085a009e612c55352025c9853ce
Alfie Fairbank <109432346+Zentifyyy@users.noreply.github.com>
2025-07-14 20:12:15 +0100

    added some starting functionality

--- Task Tracker.cpp
+++ Task Tracker.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+std::string lastCommand;
+
+std::string Commandinput() {
+    std::string text;
+    std::cin >> text;
+
+    if(text[0] != '/'){
+        std::cout << "Please type a command.";
+        return "";
+    }
+
+    lastCommand = text;
+
+    return text;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!, Welcome to your personal Task Tracker!\n";
+
+    std::cout << "What would you like to do?\n";
+
+    std::cout << "Use /help for a list of commands.\n";
+
+    if (Commandinput() == "/help") {
+        std::cout << "/task - shows all current tasks\n";
+    }
+
+    if (lastCommand == "/task") {
+        std::cout << "Here are all of your tasks!\n";
+    }
 }
